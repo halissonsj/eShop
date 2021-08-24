d256ecd79f0df394a0533e4abe6ccde11114d204
Halisson da Silva Jesus <hsjesus@uolinc.com>
2021-08-24 19:29:44 -0300

    Initial Commit

---
+++ eShop/eShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  eShopApp.swift
+//  eShop
+//
+//  Created by Halisson da Silva Jesus on 24/08/21.
+//
+
+import SwiftUI
+
+@main
+struct eShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
